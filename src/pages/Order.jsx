48ccea1a454ec52f889ba08ded0a3e131d8c7d39
abhishekkjainn <jainabhishek1904@gmail.com>
2024-06-22 19:39:51 +0530

    Added the order Section page responsive Design functionalities yet to be added

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -5,6 +5,41 @@ export default function Order() {
         <div className="headingtag orderheadingtag">All Orders</div>
         <div className="filter">Filter</div>
       </div>
+
+      <div className="ordersection">
+        <Orderpagecard />
+        <Orderpagecard />
+        <Orderpagecard />
+        <Orderpagecard />
+        <Orderpagecard />
+      </div>
     </div>
   );
+
+  function Orderpagecard() {
+    return (
+      <div className="orderpagecard">
+        <div className="snoimage">
+          <div className="sno">1.</div>
+          <img src="prod1.jpg" alt="" className="prodimageorderpage" />
+        </div>
+        <div className="idquantity">
+          <div className="orderpageid">ID - 123ABC</div>
+          <div className="quantity">Count - 2</div>
+        </div>
+        <div className="custnameorderpage idquantity">
+          <div className="orderpageid">Customer</div>
+          <div className="quantity">Abhishek Jain</div>
+        </div>
+        <div className="buttonsection">
+          <div className="status">
+            {' '}
+            <div className="redcircle"></div> Pending
+          </div>
+          <div className="markasdonebutton">Mark Done</div>
+          <div className="rejectorderbutton">Reject Order</div>
+        </div>
+      </div>
+    );
+  }
 }
